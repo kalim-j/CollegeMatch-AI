9c5b28383a707b30a551010fb3b0f8d030119942
https://github.com/kalim-j <kalimdon07@gmail.com>
2026-06-24 17:30:28 +0530

    Fix Firebase Auth popup issue by setting COOP to unsafe-none

--- next.config.ts
+++ next.config.ts
@@ -106,7 +106,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin-allow-popups',
+            value: 'unsafe-none',
           },
           {
             key: 'Cross-Origin-Embedder-Policy',
